a2c43ef8f68249a47a4b928347ce2f90002cac35
CodeWithDev15 <rehbarkhan1155@gmail.com>
2026-01-06 23:06:51 +0530

    Add files via upload

---
+++ Comments,_SpaceSequences,&PrintStatement.py
@@ -0,0 +1,61 @@
+# print("Hello World 
+#       and Hello to me")  
+# 
+# # The code won't work if you try to a space, for space we will use Space Squence Chracters so for that, use back slash n \ and literally you don't have to add a space for new just use this: \
+
+print("Hello World\nHello To Me!") # After writing some chracters, if u add space then write \n it will do chnages in the new line and after \n chracters will move one space ahead.
+
+#If we want to tell reader of the code that hey don't remove this or copy this function or inhrent for that we will use the fucntion of comments by using # or commend + forward slash /
+
+# This is a comment
+
+# We can also write a comment after the code and remember not before like this
+
+print("H") # A exmaple
+# a example of not going read the code print("H")
+
+# Alt + down arrow key to move below of the code lines
+
+# Tripple single quotes are also useful for comments if you don't want to use commend/contorl + forward slash or a hastag #
+
+# You can comment also uncomment.
+
+'''sdsd''' # Is also a comment and this is called multi line comment and also other also are multi line comment 
+
+"""sdsd""" # This is a example of triple double quote
+
+"""A example of if else statement"""
+
+x = 10
+if x> 5:
+    print("The {x} is greater!")
+else:
+    print("{x} is smaller than {5}")
+
+# If you add a new line into a string, it can not parse so we need to use or take help of new line character which is \n
+
+# Inside double quotes if you want to write something, which contains a double quotes so their you need to do is when the need of double quotes instead that use single quote because python understand that double quotes means the code is going to end but again their is some code so it will give you a error, so we use single code or we can creating our sting into single quote also or can use "\" before the end quote use back slash  and also before highlting the double quote text use back slash both times, Look below example.
+
+print('hello world and who said that? "Pro. Rehbar Khan" said it.')
+# This below Example for back slash double quote the below one.
+print("Hello World is said by who? \"Pro. Rehbar Khan\" as said it.") # <- Yep, this one! Furthermore, it also works with single code.
+print('Hello is said by who? \'Pro. Rehbar Khan\' said it')
+# What is a back slash a Escape Sequence Chracter. 
+
+# We can just not only print a single value for eg: a string, we can also print different and multiple tupes of value by just using a comma "," Yep.
+
+print("A String", 5, "<- It is a number as you can see", 0)
+
+# if we want to joined more than a one value together by mean I mean with string we can use seperator.
+
+print("Hello", "Two", "You", sep="~") # Yep like this, for sep="", we use individual doubles quotes because we are using or calling multiple different types values so from that I mean we alread used and called string and then, another string, than sep="inside use '~' 'Yes'"
+
+# end="007\n" <- A quest to fingure out. An optional to teach! But this a doubut for use case of end="'Enter a number or etc'\n"
+
+print("Hello", 7, 9, sep="~", end="007\n") 
+
+print("Hi", "Hi,", sep="~", end="007\n")
+
+
+
+
