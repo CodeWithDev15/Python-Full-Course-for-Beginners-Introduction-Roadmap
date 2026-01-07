92e416b679f517952aeab9f70b5ccfe62ca2d124
CodeWithDev15 <rehbarkhan1155@gmail.com>
2026-01-07 21:24:44 +0530

    Add files via upload

---
+++ Containers.py
@@ -0,0 +1,16 @@
+print("Welcome to container finder.")
+
+container = ["Yellow Daal", "Black Dal", "Normal Salt", "Black Tea", "Green Tea"
+]
+container_two = ["Orange", "Mango"]
+
+input("Enter the address of the container ") # After writing the promot, kindly add a sapace, when it ask from user their will be a space so it is very better for visble.
+
+x = container
+y = container_two
+
+if x == input():
+    print(x)
+elif y == input():
+    print(y)
+
