c119693ed88102cd777ee6ebe206e7db3907cb5a
CodeWithDev15 <rehbarkhan1155@gmail.com>
2026-01-08 22:39:24 +0530

    Add files via upload

---
+++ Variables_Date_Types.py
@@ -0,0 +1,58 @@
+# Data Types 
+
+a = 1 # A integer 
+b = False # A Boolen
+c = "1" # We use double quote to represent our string and the date inside it a text
+d = None # None type .
+
+print(a+c) # If you print this, you goona see an error, because you cannot add a string which is a text with  an int or uint.
+
+# You can concatenate it!
+
+# What is a Data Type?
+
+# Data type speicfy that type of value a variable holds 
+
+# To check the type of variable, what type of is it? We will use type('Enter variable here')
+
+print("The type of a is ", type(a))
+
+# type is unsupported.
+
+# Python has inbuild data types, for eg:
+# Numeric data: int, float, complax
+
+#Must study complax numbers, for each Board the sllabbus is kinda different. Class 11th or 12 has
+
+# When you have to hear the answer is Yes or No, then use Boolen.
+
+# What is list? Collection of different data elements/types [] Notaion
+
+# Tuple are inmutables () Notaion to call
+
+# Dictionary {} Notaion.  It is use for key-value-pairs 
+
+# Study about Mutations. Study about Genetic Algorithm
+
+# A Array Code :
+
+a_a = ["This is a array", 10, False, "String"]
+print(a_a)
+
+# A Truple Code:
+
+b_b = ("It is unmutable", True, 100, "%")
+print(b_b)
+
+# A Dictionary Code:
+
+c_c = {
+    "name":"Rehbar Khan", 
+    "class":"XIII",
+    "college_location":"deutschland"
+}
+
+print(c_c)
+# Everything in Python is object, class object, what is class? we will later study about it in object oriented programing language.
+
+# Make sure to study effciently about data types and grasp it.
