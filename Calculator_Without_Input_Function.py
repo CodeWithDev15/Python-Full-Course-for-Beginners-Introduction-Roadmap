402e27cef9688373111aa761bbdc3efa8166ce12
CodeWithDev15 <rehbarkhan1155@gmail.com>
2026-01-09 22:53:30 +0530

    Add files via upload

---
+++ Calculator_Without_Input_Function.py
@@ -0,0 +1,10 @@
+a = 10
+b = 9
+
+print("The sum of", a, "+", b, "is ", a+b)
+print("The sum of", a, "-", b, "is ", a-b)
+print("The sum of", a, "*", b, "is ", a*b)
+print("The sum of", a, "/", b, "is ", a/b)
+print("The sum of", a, "//", b, "is ", a//b)
+print("The sum of", a, "%", b, "is ", a%b)
+print("The sum of", a, "**", b, "is ", a**b)
