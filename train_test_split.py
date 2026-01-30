8d1af4cdf2c5428f297bf159325848b33dc02db4
CodeWithDev15 <rehbarkhan1155@gmail.com>
2026-01-31 00:02:22 +0530

    Add files via upload

---
+++ train_test_split.py
@@ -0,0 +1,90 @@
+# import pandas as pd
+# from sklearn.datasets import load_iris
+# iris = load_iris()
+
+# iris.feature_names
+# iris.target_names
+
+# df =pd.DataFrame(iris.data,columns=iris.feature_names)
+# df.head()
+
+# df["target"] = iris.target
+
+# df.head()
+
+# df["flower_names"] = df.target.apply(lambda x: iris.target_names[x])
+# df.head()
+
+# df[45:50]
+# df[95:100]
+# df[145:150]
+
+# df0 = df[:50]
+# df1 = df[50:100]
+# df2 = df[100:150]
+
+# import matplotlib.pyplot as plt
+# #%matplotlib inline 
+
+# plt.xlabel("speal length")
+# plt.xlabel("sepal width")
+# plt.scatter(df0["sepal length (cm)"], df0["sepal width (cm)"], color="red", marker="+")
+# plt.scatter(df1["sepal length (cm)"], df0["sepal width (cm)"], color="black", marker="+")
+
+# plt.xlabel("sepal length (cm)")
+# plt.ylabel("sepal width (cm)")
+
+# plt.legend()
+
+# plt.show()
+
+
+# plt.xlabel("patel length (cm)")
+# plt.xlabel("patel width (cm)")
+# plt.scatter(df0["patel length (cm)"],df0["patel width (cm)"],color="green",marker="+")
+# plt.scatter(df1["patel length (cm)"],df0["patel width (cm)"],color="green",marker=".")
+
+# plt.xlabel("patel length (cm)")
+# plt.ylabel("patel width (cm)")
+
+# plt.legend()
+
+# plt.show()
+
+import pandas as pd
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+%matplotlib inline 
+
+iris = load_iris()
+
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df["target"] = iris.target
+df["flower_names"] = df.target.apply(lambda x: iris.target_names[x])
+
+# Separate each flower type
+df0 = df[:50]
+df1 = df[50:100]
+df2 = df[100:150]
+
+# --- First scatter plot (sepal) ---
+plt.xlabel("sepal length (cm)")
+plt.ylabel("sepal width (cm)")
+
+plt.scatter(df0["sepal length (cm)"], df0["sepal width (cm)"], color="red", marker="+", label="Setosa")
+plt.scatter(df1["sepal length (cm)"], df1["sepal width (cm)"], color="blue", marker=".", label="Versicolor")
+plt.scatter(df2["sepal length (cm)"], df2["sepal width (cm)"], color="green", marker="*", label="Virginica")
+
+plt.legend()
+plt.show()
+
+# --- Second scatter plot (petal) ---
+plt.xlabel("petal length (cm)")
+plt.ylabel("petal width (cm)")
+
+plt.scatter(df0["petal length (cm)"], df0["petal width (cm)"], color="red", marker="+", label="Setosa")
+plt.scatter(df1["petal length (cm)"], df1["petal width (cm)"], color="blue", marker=".", label="Versicolor")
+plt.scatter(df2["petal length (cm)"], df2["petal width (cm)"], color="green", marker="*", label="Virginica")
+
+plt.legend()
+plt.show()
